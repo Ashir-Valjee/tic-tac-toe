a0fa202ecf314f54c79d97f4444635496b7807ac
ashir-valjee <ashir@eco-motive.com>
2025-09-03 14:07:12 +0100

    added player info to layout

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.example.tictactoe
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +51,8 @@ fun TicTacToeGame(modifier: Modifier = Modifier) {
         modifier = Modifier
         .fillMaxSize()
         .background(Color.Cyan)) {
+
+//        title area
         Box(
             modifier = Modifier
                 .fillMaxWidth()) {
@@ -51,11 +62,67 @@ fun TicTacToeGame(modifier: Modifier = Modifier) {
                 textAlign = TextAlign.Center,
                 text = stringResource(R.string.title))
         }
+
+//        game area
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color.Red)
                 .weight(1f)) {
+        }
+
+//        player info
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()) {
+
+//            player 1 info
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                Text(
+                    modifier = Modifier,
+
+
+                    text = "player 1: Ash"
+                )
+                Box(modifier = Modifier
+                    .size(25.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color.hsl(0f,1f, .76f))
+                ) {
+
+                }
+
+            }
+
+            //            player 2 info
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = Modifier,
+
+
+                    text = "player 2: Liza"
+                )
+                Box(modifier = Modifier
+                    .size(25.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color.hsl(131f, 0.52f, 0.50f))
+                ) {
+
+                }
+
+            }
         }
     }
 }
